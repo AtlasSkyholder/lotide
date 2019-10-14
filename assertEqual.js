d0860d7c59c9490a4976b90fa048263617629550
Fred <thecanuckatheist@gmail.com>
2019-10-14 19:58:36 +0000

    assertEqual.js exercise

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+//const errorMsg = " !== ";
+//const msg = " === ";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Porsche", "Porsche");
+assertEqual(3, 9);
