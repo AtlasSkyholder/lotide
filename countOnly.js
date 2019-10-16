f14a85953819d7c6aeb316f4cab060d390f277ec
Fred <thecanuckatheist@gmail.com>
2019-10-16 17:07:21 +0000

    count number of instances element shows in array and returns the elements and how many times it shows up as an object

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let objComp = {};
+  for (let item in itemsToCount) {
+    objComp[item] = 0;
+  }
+  for (let word of allItems) {
+    if (word in objComp) {
+      objComp[word] += 1;
+    }
+  }
+  return objComp;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+console.log(result1);
+//assertEqual(result1["Jason"], 1);
+//assertEqual(result1["Karima"], undefined);
+//assertEqual(result1["Fang"], 2);
