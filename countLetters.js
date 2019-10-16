cf43d2cd85908f23a536a9837b61f8a6d1553c52
Fred <thecanuckatheist@gmail.com>
2019-10-16 17:31:30 +0000

    function that counts letters in a string and returns an object with individual letters and the number of times it shows up

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let countLetters = function(str) {
+  let longWord = str.split(' ').join('');
+  let objLetter = {};
+  for (let single of longWord) {
+    if (single in objLetter) {
+      objLetter[single] += 1;
+    } else {
+      objLetter[single] = 1;
+    }
+  }
+  return objLetter;
+};
+
+console.log(countLetters("lighthouse in the house"));
+
+//assertEqual(countLetters("word"), {'w' : 1 , 'o' : 1 , 'r' : 1 , 'd' : 1});
