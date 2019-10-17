ba0808ec983fbf7930c6628eccb9e49ff2f10041
Fred <thecanuckatheist@gmail.com>
2019-10-17 17:53:03 +0000

    final version of map.js

--- map.js
+++ map.js
@@ -1,14 +1,43 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
+const eqArrays = function(arrOne, arrTwo) {
+  let count = 0;
+  if (arrOne.length === arrTwo.length) {  // checks if the arrays are different length
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] === arrTwo[i]) {
+        count++;
+      } else {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  if (count === arrOne.length) {
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let value = eqArrays(arrOne, arrTwo);
+  if (value) {
+    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
 const map = function(array, callback) {
   // empty for now :)
-  console.log('array: ', array);
-  console.log('callback: ', callback);
-
   const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
   return results;
-}
+};
 
 const results1 = map(words, word => word[0]);
 
 console.log(results1);
+
+assertArraysEqual(results1,['g', 'c', 't', 'm', 't']);
