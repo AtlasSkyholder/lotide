d7ff72b0a93f419164dc83d7554af647bb711af7
Fred <thecanuckatheist@gmail.com>
2019-10-17 19:22:37 +0000

    Final version with no errors

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  let size = Object.keys(obj).length;
+  let count = 0;
+  for (let item in obj) {
+    if (callback(obj[item])) {
+      return item;
+    }
+    count++;
+  }
+  if (count === size) {
+    return undefined;
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'); // Pass
+
+assertEqual(findKey({
+  "Impreza": { drivetrain: 4},
+  "Corolla": { drivetrain: 2},
+  "Civic": { drivetrain: 2},
+  "Rav4": { drivetrain: 4}
+}, x => x.drivetrain === 2), 'F-150'); // Fail
+
+assertEqual(findKey({
+  "Impreza": { drivetrain: 4},
+  "Corolla": { drivetrain: 2},
+  "Civic": { drivetrain: 2},
+  "Rav4": { drivetrain: 4}
+}, x => x.drivetrain === 3), 'F-150'); // Fail => undefined
