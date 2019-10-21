a48ba581058a792877f7c823c92dc03f3ac7698e
Fred <thecanuckatheist@gmail.com>
2019-10-21 17:55:26 +0000

    modified it to function with mocha and chai

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let tail = function(words) {
   let newArr = [];
@@ -23,10 +17,12 @@ let tail = function(words) {
 };
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const empty = [];
-const single = ["Stuff"];
-tail(words); // no need to capture the return value since we are not checking it
-tail(empty);
-tail(single);
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//const empty = [];
+//const single = ["Stuff"];
+//tail(words); // no need to capture the return value since we are not checking it
+//tail(empty);
+//tail(single);
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
