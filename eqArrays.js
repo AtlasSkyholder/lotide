605b3e749e73cd274b194191c2e343902bf8b332
Fred <thecanuckatheist@gmail.com>
2019-10-21 23:30:57 +0000

    fixed eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -2,9 +2,11 @@ const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrOne, arrTwo) {
   let count = 0;
-  if (arrOne.length === arrTwo.length) {  // checks if the arrays are different length
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] === arrTwo[i]) {
+  let superArr = buildArr(arrOne);
+  let superArrTwo = buildArr(arrTwo);
+  if (superArr.length === superArrTwo.length) {  // checks if the arrays are different length
+    for (let i = 0; i < superArr.length; i++) {
+      if (superArr[i] === superArrTwo[i]) {
         count++;
       } else {
         return false;
@@ -13,16 +15,31 @@ const eqArrays = function(arrOne, arrTwo) {
   } else {
     return false;
   }
-  if (count === arrOne.length) {
+  if (count === superArr.length) {
     return true;
   }
 };
 
+const buildArr = function(array) {
+  let result = [];
+  array.forEach(function(value) {
+    if(Array.isArray(value)) {
+        result = result.concat(buildArr(value));
+    } else {
+        result.push(value);
+    }
+  });
+
+return result;
+};
+
 module.exports = eqArrays;
 
-//eqArrays([1, 2, 3], [1, 2, 3]) // => true
-//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+// console.log(eqArrays([1, 2, [3, 4]], [1, 2, [3, 4]]))
 
