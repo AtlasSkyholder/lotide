80844873b3eac47937827941c6c29df650a94ba4
Fred <thecanuckatheist@gmail.com>
2019-10-21 17:52:33 +0000

    modified it to function with mocha and chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,27 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let count = 0;
-  if (arrOne.length === arrTwo.length) {  // checks if the arrays are different length
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] === arrTwo[i]) {
-        count++;
-      } else {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  if (count === arrOne.length) {
-    return true;
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrOne, arrTwo) {
-  let value = eqArrays(arrOne, arrTwo);
-  if (value) {
-    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
-  } else {
-    console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
-  }
+//   let count = 0;
+//   if (arrOne.length === arrTwo.length) {  // checks if the arrays are different length
+//     for (let i = 0; i < arrOne.length; i++) {
+//       if (arrOne[i] === arrTwo[i]) {
+//         count++;
+//       } else {
+//         return false;
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+//   if (count === arrOne.length) {
+//     return true;
+//   }
+// };
+  let result = eqArrays(arrOne, arrTwo);
+  return assertEqual(result, true);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+
+module.exports = assertArraysEqual;
